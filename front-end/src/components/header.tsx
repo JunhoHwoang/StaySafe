5fb88f2ff37be9e787fbe61baec92c9ddf67161f
Akshat2923 <akshat.saladi@gmail.com>
2024-10-19 16:17:33 -0400

    padding

--- front-end/src/components/header.tsx
+++ front-end/src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="w-full p-4 flex items-center justify-center">
+    <div className="w-full p-4 flex items-center justify-center border-b">
       <Link to="/">
         <p className="text-3xl">StaySafe</p>
       </Link>
