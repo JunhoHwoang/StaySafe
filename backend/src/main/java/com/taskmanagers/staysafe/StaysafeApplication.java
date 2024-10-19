85e39d58c8cf15e800e829e3582d1788830520e9
donutdiet <72674098+donutdiet@users.noreply.github.com>
2024-10-19 13:16:56 -0400

    Implement basic logic for parsing .csv file data

--- backend/src/main/java/com/taskmanagers/staysafe/StaysafeApplication.java
+++ backend/src/main/java/com/taskmanagers/staysafe/StaysafeApplication.java
@@ -1,13 +1,33 @@
 package com.taskmanagers.staysafe;
 
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvException;
+import com.taskmanagers.staysafe.domain.Incident;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class StaysafeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StaysafeApplication.class, args);
+	public static void main(String[] args) throws IOException, CsvException {
+		//SpringApplication.run(StaysafeApplication.class, args);
+		ClassLoader classLoader = StaysafeApplication.class.getClassLoader();
+		File file = new File(Objects.requireNonNull(classLoader.getResource("data/incidents.csv")).getFile());
+
+		List<Incident> incidents = new CsvToBeanBuilder<Incident>(new FileReader(file))
+				.withType(Incident.class)
+				.build()
+				.parse();
+
+		incidents.forEach(System.out::println);
 	}
 
 }
