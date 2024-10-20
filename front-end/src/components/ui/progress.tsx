848d88eae8334daccb30e19c228fd19a8593ae0d
JunhoHwoang <154561586+JunhoHwoang@users.noreply.github.com>
2024-10-19 22:19:27 -0400

    Pagination color for dark mode

--- front-end/src/components/ui/progress.tsx
+++ front-end/src/components/ui/progress.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
-
+// Define the Progress component with a value prop for percentage
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { value: number }
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -16,11 +16,11 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-red-600 transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className="h-full bg-red-600 transition-transform duration-300 ease-in-out"
+      style={{ width: `${value}%` }}
     />
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
 
-export { Progress }
+export { Progress };
