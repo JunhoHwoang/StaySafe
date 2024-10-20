f52f201146f9923245bb9c5d313c0e01a8ad142b
JunhoHwoang <154561586+JunhoHwoang@users.noreply.github.com>
2024-10-19 21:53:26 -0400

    changed graphs to statistics

--- front-end/src/components/mainPage.tsx
+++ front-end/src/components/mainPage.tsx
@@ -64,7 +64,7 @@ export default function MainPage() {
       <Header />
       <div className="flex-1 grid grid-cols-1 md:grid-cols-4 gap-6 p-6">
         <div className="md:col-span-2 order-last md:order-first space-y-4">
-          <h2 className="text-2xl font-semibold">Graphs</h2>
+          <h2 className="text-2xl font-semibold">Statistics</h2>
           <Graphs cardData={cardData} />
           <GraphStats cardData={cardData} />
         </div>
